aee4cef26d1b6f26f7b99cc388ec9a1ff0eaa43e
Intan Paoli <intankhairunnisa@gmail.com>
2021-01-20 18:05:59 +0100

    .env

--- build/precache-manifest.9247e84417c9566218126453e26325eb.js
+++ build/precache-manifest.9247e84417c9566218126453e26325eb.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d5976063bff8a0003bc2b0d8d5d23083",
+    "revision": "579314d40c6f02f2a427791e344b056f",
     "url": "/index.html"
   },
   {
-    "revision": "7bd6a3bde8afcf88b3be",
+    "revision": "cf2a400835d6ba41c3c0",
     "url": "/static/css/main.ccb02f88.chunk.css"
   },
   {
-    "revision": "c2c84bee03c802f94314",
-    "url": "/static/js/2.2ad88393.chunk.js"
+    "revision": "75b433de717f735f1e02",
+    "url": "/static/js/2.a9da00dd.chunk.js"
   },
   {
     "revision": "9b318b6fb13190fe82c0677e9264b3c7",
-    "url": "/static/js/2.2ad88393.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.a9da00dd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7bd6a3bde8afcf88b3be",
-    "url": "/static/js/main.42ce6212.chunk.js"
+    "revision": "cf2a400835d6ba41c3c0",
+    "url": "/static/js/main.ab13578a.chunk.js"
   },
   {
     "revision": "9b4e396c183e42c1fa5c",
